4faa108be0c7780c80b849da873d0a6cd125e945
Dree20 <dreannaad@gmail.com>
2025-08-26 13:01:20 -0400

    Added to index.html and index.js

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ function updateTime() {
   let losAngelesTimeElement = document.querySelector(".time");
   let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do,YYYY");
   losAngelesTimeElement.innerHTML = losAngelesTime.format(
     " h:mm:ss [<small>] A [</small>]"
   );
