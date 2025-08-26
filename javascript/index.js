2bfb8390ada880d1a6df41674395fecdf46121cb
Dree20 <dreannaad@gmail.com>
2025-08-26 14:56:32 -0400

    Added my location to index.js and removed some elements from index.html

--- javascript/index.js
+++ javascript/index.js
@@ -5,7 +5,7 @@ function updateTime() {
     let losAngelesTimeElement = document.querySelector(".time");
     let losAngelesTime = moment().tz("America/Los_Angeles");
 
-    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM   Do, YYYY");
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM   Do YYYY");
     losAngelesTimeElement.innerHTML = losAngelesTime.format(
       " h:mm:ss [<small>] A [</small>]"
     );
@@ -16,7 +16,7 @@ function updateTime() {
     let parisTimeElement = document.querySelector("#time");
     let parisTime = moment().tz("Europe/Paris");
 
-    parisDateElement.innerHTML = parisTime.format("MMMM   Do, YYYY");
+    parisDateElement.innerHTML = parisTime.format("MMMM   Do YYYY");
     parisTimeElement.innerHTML = parisTime.format(
       " h:mm:ss [<small>] A [</small>]"
     );
@@ -24,6 +24,9 @@ function updateTime() {
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -31,7 +34,7 @@ function updateCity(event) {
   <div class="city">
 <div> 
 <h2>${cityName}</h2> 
-<div class= "date">${cityTime.format("MMMM   Do, YYYY")} </div>
+<div class= "date">${cityTime.format("MMMM   Do YYYY")} </div>
 </div>
 <div class="time">${cityTime.format("h:mm:ss ")}  <small>${cityTime.format(
     "A"
